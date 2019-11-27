942d194603a98242378c9ce6ab1586a9cca80a1c
CCutecute <897996795@qq.com>
2019-11-27 16:23:31 +0800

    fisrt test

---
+++ src/main/scala/com/ad/ReleaseConstant.scala
@@ -0,0 +1,16 @@
+package com.ad
+
+/**
+  * Description:
+  * Copyright (c),2019,JingxuanYan 
+  * This program is protected by copyright laws. 
+  *
+  * @author 闫敬轩
+  * @date 2019/11/26 14:50
+  * @version 1.0
+  */
+class ReleaseConstant {
+
+
+
+}
